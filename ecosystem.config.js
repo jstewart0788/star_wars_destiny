8786c6f45a7d1945812beefe9af2fd6b64a107f1
Jonathan Stewart <jstewart0788@gmail.com>
2017-02-01 23:21:01 -0500

    add configuration file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name        : "starwars",
+    script      : "./bin/www",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+      "PORT": "8080"      
+    },
+    env_production : {
+       "NODE_ENV": "production",
+       "PORT": "8080"
+    }
+  }]
+}
+
